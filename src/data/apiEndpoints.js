99734033e3a2492ff22e44258278092821ad3e0d
Valerie Lui <vallq@users.noreply.github.com>
2020-02-15 15:53:11 +0800

    Added API endpoints to separate file

---
+++ src/data/apiEndpoints.js
@@ -0,0 +1,12 @@
+const apiEndpoints = {
+  "0": "GET    /",
+  "1": "GET   /pokemon",
+  "2": "GET   /pokemon?name=pokemonNameNotExact",
+  "3": "POST    /pokemon",
+  "4": "GET /pokemon/:id",
+  "5": "PUT /pokemon/:id",
+  "6": "PATCH /pokemon/:id",
+  "7": "DELETE /pokemon/:id"
+};
+
+module.exports = apiEndpoints;
