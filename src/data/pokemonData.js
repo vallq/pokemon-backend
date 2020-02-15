55244351e3d436d77ffbf40365c56ad3df205c7a
Valerie Lui <vallq@users.noreply.github.com>
2020-02-15 15:53:41 +0800

    Added seed pokemonData

---
+++ src/data/pokemonData.js
@@ -0,0 +1,16 @@
+const pokemonData = [
+  {
+    id: 1,
+    name: "Pikachu",
+    japaneseName: "ピカチュウ",
+    baseHP: 35,
+    category: "Mouse Pokemon"
+  },
+  {
+    id: 2,
+    name: "Squirtle",
+    japaneseName: "ゼニガメ",
+    baseHP: 44,
+    category: "Tiny Turtle Pokemon"
+  }
+];
