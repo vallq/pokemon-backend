a7abbd81a9d463beed99624d4c35da7d23cd69cd
Valerie Lui <vallq@users.noreply.github.com>
2020-02-16 23:27:58 +0800

    Added tests for POST, GET/:id and PATCH:/id

--- __tests__/pokemon.route.test.js
+++ __tests__/pokemon.route.test.js
@@ -74,8 +74,50 @@ describe("pokemon", () => {
       const { body: response } = await request(app)
         .get(`/pokemon?name=chu`)
         .expect(200);
-      console.log(response);
       expect(response).toMatchObject(expectedPokemon);
+    });
+
+    it("POST / should return pokemon that has been posted", async () => {
+      const expectedPokemon = {
+        id: 3,
+        name: "Charmander",
+        japaneseName: "ヒトカゲ",
+        baseHP: 39,
+        category: "Tiny Lizard Pokemon"
+      };
+      const { body: response } = await request(app)
+        .post("/pokemon")
+        .send(expectedPokemon)
+        .expect(201);
+      expect(response).toMatchObject(expectedPokemon);
+    });
+
+    describe("/pokemon/:id", () => {
+      it("GET / should return Pokemon with corresponding ID", async () => {
+        const expectedPokemon = {
+          id: 2,
+          name: "Squirtle",
+          japaneseName: "ゼニガメ",
+          baseHP: 44,
+          category: "Tiny Turtle Pokemon"
+        };
+        const { body: response } = await request(app)
+          .get(`/pokemon/${expectedPokemon.id}`)
+          .expect(200);
+        expect(response).toMatchObject(expectedPokemon);
+      });
+
+      it("PATCH / should return Pokemon with updated details", async () => {
+        const expectedPokemon = {
+          id: 1,
+          category: "Tiny Mouse Pokemon"
+        };
+        const { body: response } = await request(app)
+          .patch(`/pokemon/${expectedPokemon.id}`)
+          .send(expectedPokemon)
+          .expect(200);
+        expect(response.category).toEqual(expectedPokemon.category);
+      });
     });
   });
 });
