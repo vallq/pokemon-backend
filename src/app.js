5ecd2611edc1a99db96cc5806043d94fa845c9f0
Valerie Lui <vallq@users.noreply.github.com>
2020-02-16 17:01:28 +0800

    Modified app.js to pass test

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
 const apiEndpoints = require("./data/apiEndpoints");
+const pokemonRouter = require("./routes/pokemon.route");
 
 app.use(express.json());
+app.use("/pokemon", pokemonRouter);
 
 app.get("/", (req, res) => {
   res.status(200).send(apiEndpoints);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500);
+  console.log(err);
+  if (err.statusCode) {
+    res.send({ error: err.message });
+  } else {
+    res.send({ error: "internal server error" });
+  }
 });
 
 module.exports = app;
