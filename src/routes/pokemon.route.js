950252e15557f6d6f496aef2df7c437bc18f8622
Valerie Lui <vallq@users.noreply.github.com>
2020-02-16 17:00:11 +0800

    Modified route to pass test

--- src/routes/pokemon.route.js
+++ src/routes/pokemon.route.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const pokemonData = require("../data/pokemonData");
+const BasicPokemon = require("../models/pokemon.model");
+
+const filterByName = async searchTerm => {
+  const regexFilter = new RegExp(searchTerm, "gi");
+  const filteredPokemon = await BasicPokemon.find({ name: regexFilter });
+  return filteredPokemon;
+};
 
 router.get("/", async (req, res) => {
-  res.status(200).send(pokemonData);
+  if (req.query == "") {
+    const pokemon = await BasicPokemon.find();
+    console.log(pokemon);
+    console.log(filterByName("chu"));
+    res.status(200).send(pokemon);
+  } else {
+    const searchTerm = req.query.name;
+    const expectedPokemon = await filterByName(searchTerm);
+    res.status(200).send(expectedPokemon);
+  }
 });
-
 module.exports = router;
